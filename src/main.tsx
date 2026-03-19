50ff29950e8d082d23f6f51dedd6ccf3500f264b
gualask <gualask.dev@gmail.com>
2026-03-19 09:16:58 +0100

    changed font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "@fontsource-variable/public-sans";
+import "@fontsource-variable/plus-jakarta-sans";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
